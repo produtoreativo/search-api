bb26f1bbfed88b9f21e8df6ae7cb568341c0167d
Diogo Chaves <diogochaves@me.com>
2023-05-13 11:58:35 -0300

    test init

--- nest-search-api/src/elastic/elastic.module.ts
+++ nest-search-api/src/elastic/elastic.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { ElasticService } from './elastic.service';
+import { ElasticController } from './elastic.controller';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ElasticService } from './elastic.service';
     }),
   ],
   providers: [ElasticService],
+  controllers: [ElasticController],
   exports: [ElasticService],
 })
 export class ElasticModule {}
