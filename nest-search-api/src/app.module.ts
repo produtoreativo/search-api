0c5f56b44491eb6d9192b37d72fa73ecc9b4b5c2
Christiano Milfont <cmilfont@gmail.com>
2023-05-15 06:12:00 -0300

    Buscando env vars via contexto de execução em vez de pegar aquivo .env

--- nest-search-api/src/app.module.ts
+++ nest-search-api/src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ElasticModule } from './elastic/elastic.module';
-import { ElasticController } from './elastic/elastic.controller';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}),ElasticModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true,
+    }),
+    ElasticModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
